72ef0f6c768076f4de02321ae29cfd9603731b80
LordofCreepers <vip.panzarhero173z@yandex.ru>
2024-03-20 17:58:11 +0400

    Added prototype of 'Parse' method

--- Parser.cpp
+++ Parser.cpp
@@ -2,7 +2,17 @@
 #include "Parser.hpp"
 #include "Exceptions.hpp"
 
-void Parser::Analyze(const std::string& in_expression, std::vector<TokenPtr>& out_tokens)
+void Parser::AddFactory(TokenFactory&& factory)
+{
+	TokenFactories.emplace_back(factory);
+}
+
+void Parser::SetComparator(TokenComparator&& comparator)
+{
+	TokenComparator = comparator;
+}
+
+void Parser::Tokenize(const std::string& in_expression, std::vector<TokenPtr>& out_tokens)
 {
 	out_tokens.clear();
 
@@ -23,7 +33,47 @@ void Parser::Analyze(const std::string& in_expression, std::vector<TokenPtr>& ou
 	}
 }
 
-std::vector<TokenFactory>& Parser::GetTokenFactories()
+void Parser::Parse(const std::vector<TokenPtr>& tokens, TreeNode<TokenPtr>& ast_root)
+{
+	ast_root.Value.reset();
+	ast_root.Children.clear();
+
+	for (const TokenPtr& token : tokens)
+	{
+		
+	}
+}
+
+const std::vector<TokenFactory>& Parser::GetTokenFactories() const
 {
 	return TokenFactories;
+}
+
+const TokenComparator& Parser::GetTokenComparator() const
+{
+	return TokenCompare;
+}
+
+void Parser::AddTokenFactory(const TokenFactory& factory)
+{
+	if (!factory) throw InvalidFactory{};
+
+	AddFactory(TokenFactory(factory));
+}
+
+void Parser::AddTokenFactory(TokenFactory&& factory)
+{
+	if (!factory) throw InvalidFactory{};
+
+	AddFactory(TokenFactory(factory));
+}
+
+void Parser::SetTokenComparator(const TokenComparator& comparator)
+{
+	SetComparator(TokenComparator(comparator));
+}
+
+void Parser::SetTokenComparator(TokenComparator&& comparator)
+{
+	SetComparator(TokenComparator(comparator));
 }
--- Parser.hpp
+++ Parser.hpp
@@ -1,21 +1,36 @@
 #pragma once
 
+#include <string>
 #include <vector>
 #include <memory>
 #include <functional>
+#include "TreeNode.hpp"
 
 class IToken;
 using TokenPtr = std::shared_ptr<IToken>;
+
+using TokenComparator = std::function<bool (const TokenPtr&, const TokenPtr&)>;
 
 class Parser;
 using TokenFactory = std::function<TokenPtr (const Parser&, const std::string&, size_t&)>;
 
 class Parser
 {
-public:
+protected:
 	std::vector<TokenFactory> TokenFactories;
-public:
-	virtual void Analyze(const std::string&, std::vector<TokenPtr>&);
+	TokenComparator TokenCompare;
 
-	virtual std::vector<TokenFactory>& GetTokenFactories();
+	virtual void AddFactory(TokenFactory&&);
+	virtual void SetComparator(TokenComparator&&);
+public:
+	virtual void Tokenize(const std::string&, std::vector<TokenPtr>&);
+	virtual void Parse(const std::vector<TokenPtr>&, TreeNode<TokenPtr>&);
+
+	virtual const std::vector<TokenFactory>& GetTokenFactories() const;
+	virtual const TokenComparator& GetTokenComparator() const;
+
+	void AddTokenFactory(const TokenFactory&);
+	void AddTokenFactory(TokenFactory&&);
+	void SetTokenComparator(const TokenComparator&);
+	void SetTokenComparator(TokenComparator&&);
 };
