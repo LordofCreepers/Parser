9d2653fb735839208342602920c8e1fb7d61f635
LordofCreepers <vip.panzarhero173z@yandex.ru>
2024-03-20 17:58:55 +0400

    Removed mandatory comparison operations on tokens

--- Token.hpp
+++ Token.hpp
@@ -1,14 +1,3 @@
 #pragma once
 
-#include <string>
-
-class IToken
-{
-public:
-	virtual bool operator==(const IToken&) const = 0;
-	virtual bool operator!=(const IToken&) const = 0;
-	virtual bool operator>(const IToken&) const = 0;
-	virtual bool operator>=(const IToken&) const = 0;
-	virtual bool operator<(const IToken&) const = 0;
-	virtual bool operator<=(const IToken&) const = 0;
-};
+class IToken {};
