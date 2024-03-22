081c7ab6782aecba07731a6deb3587790cdf0796
LordofCreepers <34453697+LordofCreepers@users.noreply.github.com>
2024-03-22 14:30:43 +0400

    Changed the max amount of params to 2 and replaced 'PrecedenceOver' by 'IsPrecedent'

--- Token.hpp
+++ Token.hpp
@@ -1,3 +1,16 @@
 #pragma once
 
-class IToken {};
+class IToken
+{
+public:
+	enum class ParamConfig
+	{
+		NONE = 0,
+		UNARY,
+		BINARY
+	};
+
+	virtual bool IsPrecedent(const IToken*) const = 0;
+	
+	virtual ParamConfig ParamCount() const = 0;
+};
